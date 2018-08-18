ba58d822318267ad93e2e84f7f5fc7dc3754072f
AlidaContreras <alidacontreras01@gmail.com>
2018-08-17 19:09:28 -0500

    Probrando filtro de firebase

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const db = firebase.firestore();
 const speech = () => {// esta funcion es el codigo que sale en el error grandote
-	let firestore = firebase.firestore();
-	const settings = { timestampsInSnapshots: true };
-	firestore.settings(settings);
+  let firestore = firebase.firestore();
+  const settings = { timestampsInSnapshots: true };
+  firestore.settings(settings);
 }
-const readData = () => {
-  speech();
-  db.collection("placesOfBarranco").onSnapshot((querySnapshot) => {
-    console.log(querySnapshot._snapshot.query.path.segments[0]);	//
-    // boxPosteado.innerHTML = "";
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.data().place}`);
-      
-    //   // const collectionName = querySnapshot._snapshot.query.path.segments[0];
-    });
-  })
-}
-readData();
+speech();
+const inputSearch = document.getElementById('inputSearch');
 
-console.log();
+const viewData = () => {
+  db.collection("placesOfBarranco").get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      // doc.data() is never undefined for query doc snapshots
+      // console.log(doc.id, " => ", doc.data());
+    });
+  });
+}
+// viewData();
+
+const filterData = () => {
+  // Create a reference to the cities collection
+  var citiesRef = db.collection("placesOfBarranco");
+
+  // Create a query against the collection.
+  var query = citiesRef.where("name", "==", "Veggie Pizza");
+  console.log(query);
+}
+filterData();
+
+  inputSearch.addEventListener ('keyup', () => {
+    filterData();
+  })
+
