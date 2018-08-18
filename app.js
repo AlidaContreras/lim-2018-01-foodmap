90a7330ca21dbcb25674a0084c31f6fbd4737cec
AlidaContreras <alidacontreras01@gmail.com>
2018-08-18 17:17:03 -0500

    Ya filtra por tipo de comida en el select

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 const db = firebase.firestore();
+const inputSearch = document.getElementById('inputSearch');
+const selectType = document.getElementById('selectType');
+
 const speech = () => {// esta funcion es el codigo que sale en el error grandote
   let firestore = firebase.firestore();
   const settings = { timestampsInSnapshots: true };
   firestore.settings(settings);
 }
 speech();
-const inputSearch = document.getElementById('inputSearch');
 
-const viewData = () => {
+
+  
+
+selectType.addEventListener('change', () => {
   db.collection("placesOfBarranco").get().then(function (querySnapshot) {
+    viewSearch.innerHTML = "";
     querySnapshot.forEach(function (doc) {
-      // doc.data() is never undefined for query doc snapshots
-      // console.log(doc.id, " => ", doc.data());
+      doc.data() //is never undefined for query doc snapshots
+      console.log(doc.id, " => " ,doc.data().type);
+      if (selectType.value == `${doc.data().type}`) {
+        viewSearch.innerHTML += `
+        </div>
+        <br>
+        <!-- Modal Trigger -->
+        <a class="waves-effect  btn modal-trigger" href="${'#modal1'}" >Ver mas</a>
+        <!-- Modal Structure -->
+        <div id="${'modal1'}" class="${'modal'}">
+          <div class="modal-content">
+            <h4>${doc.data().name}</h4>
+            <p>Tipo de Comida: ${doc.data().type}</p>
+            <p>Lugar: ${doc.data().place}</p>
+            <p>Direcccion: ${doc.data().address}</p>
+            <p>Tipo de pago: ${doc.data().pay}</p>
+            
+          </div>
+          <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">¡Gracias!</a>
+          </div>
+        </div>
+        `
+      }
     });
   });
-}
-// viewData();
+})
+
 
 const filterData = () => {
   // Create a reference to the cities collection
@@ -25,9 +53,10 @@ const filterData = () => {
   var query = citiesRef.where("name", "==", "Veggie Pizza");
   console.log(query);
 }
-filterData();
+// filterData();
 
-  inputSearch.addEventListener ('keyup', () => {
-    filterData();
-  })
+
+inputSearch.addEventListener('keyup', () => {
+  filterData();
+})
 
