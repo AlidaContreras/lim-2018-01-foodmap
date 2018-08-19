2cb848f6f37fabf9498d352fef03dfa3c9098803
AlidaContreras <alidacontreras01@gmail.com>
2018-08-18 20:56:38 -0500

    Ya sale el splash, pero sin formato

--- parallax-template/js/app.js
+++ parallax-template/js/app.js
@@ -7,7 +7,7 @@ const speech = () => {// esta funcion es el codigo que sale en el error grandote
   const settings = { timestampsInSnapshots: true };
   firestore.settings(settings);
 }
-speech(); 
+speech();
 
 selectType.addEventListener('change', () => {
   db.collection("placesOfBarranco").get().then(function (querySnapshot) {
@@ -40,61 +40,31 @@ selectType.addEventListener('change', () => {
   });
 })
 
-search.addEventListener('keyup', () => {
+search.addEventListener('keyup', (event) => {
   db.collection("placesOfBarranco").get().then(function (querySnapshot) {
     viewSearch.innerHTML = "";
     querySnapshot.forEach(function (doc) {
       doc.data() //is never undefined for query doc snapshots
-      if (selectType.value == `${doc.data().type}`) {
+      if (event.target.value == `${doc.data().otro}`) {
         viewSearch.innerHTML += `
         </div>
         <br>
-        <!-- Modal Trigger -->
-        <a class="waves-effect  btn modal-trigger" href="${'#modal1'}" >Ver mas</a>
-        <!-- Modal Structure -->
-        <div id="${'modal1'}" class="${'modal'}">
-          <div class="modal-content">
-            <h4>${doc.data().name}</h4>
-            <p>Tipo de Comida: ${doc.data().type}</p>
-            <p>Lugar: ${doc.data().place}</p>
-            <p>Direcccion: ${doc.data().address}</p>
-            <p>Tipo de pago: ${doc.data().pay}</p>
-            
-          </div>
+         <!-- Modal Trigger -->
+          <a class="waves-effect  btn modal-trigger" href="${'#modal1'}" >Ver mas</a>
+          <!-- Modal Structure -->
+          <div id="${'modal1'}" class="${'modal'}">
+            <div class="modal-content">
+              <h4>${doc.data().name}</h4>
+              <p>Tipo de Comida: ${doc.data().type}</p>
+              <p>Lugar: ${doc.data().place}</p>
+              <p>Direcccion: ${doc.data().address}</p>
+              <p>Tipo de pago: ${doc.data().pay}</p>
+            </div>
           <div class="modal-footer">
             <a href="#!" class="modal-close waves-effect waves-green btn-flat">¡Gracias!</a>
-          </div>
         </div>
         `
       }
     });
   });
 })
-
-
-const filterData = () => {
-  db.collection("placesOfBarranco").where("name", "==", true)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
-}
-// filterData();
-
-
-inputSearch.addEventListener('keyup', () => {debugger
-  filterData();
-})
-
-// Create a reference to the cities collection
-  // var citiesRef = db.collection("placesOfBarranco");
-
-  // // Create a query against the collection.
-  // var query = citiesRef.where("name", "==", "");
-  // console.log(query);
---
+++ parallax-template/splash.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    window.setTimeout(() => {
+        splash.style.color = "#F06449";
+        window.location = "index.html";
+    }, 2000);
+}
